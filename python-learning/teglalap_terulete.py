e6b9231758590264d592fec0de2fcd39d616a765
doub83 <doub83@gmail.com>
2017-10-26 20:18:25 +0200

    Téglalap terulete

---
+++ python-learning/teglalap_terulete.py
@@ -0,0 +1,7 @@
+a = input ("a:")
+b = input ("b:")
+print ("Téglalap terület:", int(a) * int(b))
+print ("Téglalap kerülete:", int(a)*2 + int(b)*2)
+print ("a+b:", int(a) + int(b))
+print ("a-b:", int(a) - int(b))
+print ("a/b:", int(a) / int(b))
